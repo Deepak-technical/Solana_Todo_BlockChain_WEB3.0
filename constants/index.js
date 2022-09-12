82ca171c8aca41b6b63179c143ee2f63ce034f88
Deepak-mern <deepakakash156@gmail.com>
2022-09-12 16:01:16 +0530

    final devops

--- constants/index.js
+++ constants/index.js
@@ -1,3 +1,3 @@
 import { PublicKey } from "@solana/web3.js";
 
-export const TODO_PROGRAM_PUBKEY = new PublicKey("EMRELEkPnwDNsuyHmYZYmMroJ69FpdAfSgDHrTxEYrq3");
+export const TODO_PROGRAM_PUBKEY = new PublicKey("GwRnKRK3d3RgS3t8zegVNvM9E7cDj5LRJZXjRbXBMFnw");
